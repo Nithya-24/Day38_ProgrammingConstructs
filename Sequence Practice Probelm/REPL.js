3fa9ec42703802c9c5f362a2193adb1d0b018f33
Nithya-24 <nithyaramadoz@gmail.com>
2022-04-05 22:12:31 +0530

    Programming construct for Sequence Practice Problems Added

---
+++ Sequence Practice Probelm/REPL.js
@@ -0,0 +1,22 @@
+/**
+ * Question 1: Randomn Function to get Single Digit
+ **/
+console.log("Randomn Function to get Single Digit is :");
+console.log(Math.floor(Math.random() * 10));
+
+/**
+ * Question 2: Randomn to get number between 1 and 6
+ **/ 
+console.log("Randomn to get number between 1 and 6 is: ");
+console.log(Math.floor(Math.random() * 6 + 1));
+
+/**
+ * Question 3: Add 2 random dice numbers
+ **/
+ console.log("Add 2 random dice numbers is"); 
+let number1 = Math.floor(Math.random() * 6 + 1)
+console.log(number1)
+let number2 = Math.floor(Math.random() * 6 + 1)
+console.log(number2)
+let sum = number1 + number2;
+console.log("Sum is : " + sum)
